4949e1d850e2ce8f2069295d629c634ff96225d1
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2020-01-30 19:58:00 +0530

    Create TeddyAndTweety.java

---
+++ Teddy and Tweety/TeddyAndTweety.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class TeddyAndTweety {
+    public static void main(String args[] ) throws Exception {
+        Scanner S=new Scanner(System.in);
+        int N=S.nextInt();
+        if(N%3==0)
+            System.out.print("YES");
+        else
+            System.out.print("NO");
+    }
+}
