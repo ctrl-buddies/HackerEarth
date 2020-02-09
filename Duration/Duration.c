0f0df8fbd980d49fad1834550bbadf1b20ded048
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2020-02-09 10:38:32 +0530

    Update Duration.c

--- Duration/Duration.c
+++ Duration/Duration.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main() {
-  int sh,sm,eh,em,th=0,tm,tsm,tem,ttm;
-  scanf("%d %d %d %d",&sh,&sm,&eh,&em);
-  tsm=sh*60+sm;
-  tem=eh*60+em;
-  ttm=tem-tsm;
-  if(ttm%60>0)
-    th=ttm%60;
-  tm=ttm-th*60;
-  printf("%d %d",th,tm);
+    int n;
+    int sh,sm,eh,em,th,tm,ttm;
+  scanf("%d",&n);
+  while(n>0){
+      th=0;
+    scanf("%d %d %d %d",&sh,&sm,&eh,&em);
+    ttm=((eh-sh)*60)+em-sm;
+    if(ttm >= 60)
+        th=ttm/60;
+    tm=ttm-(th*60);
+    printf("%d %d\n",th,tm);
+    n--;
+  }
 }
