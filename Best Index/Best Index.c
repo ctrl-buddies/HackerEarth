2ff72616dea5c083548430f00733011b99227c2d
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2020-02-09 15:53:58 +0530

    Update Best Index.c

--- Best Index/Best Index.c
+++ Best Index/Best Index.c
@@ -4,31 +4,31 @@
 #include<stdio.h>
 void main()
 {
-    long long int n;
+    long long n;
     scanf("%lld",&n);
-    long long int a[n],b[n],i;
-    for(long long int i=0;i<n;i++)
+    long long a[100001],i,max=0;
+    for(i=0;i<n;i++)
     {
         scanf("%lld",&a[i]);
     }
+    long long t,m,k,sum;
     for(i=0;i<n;i++){
-        long long int sum=0,t=1,m,k=i;
+        k=i;
+        t=1;
+        sum=0;
         while((k+t)<=n){
-        m=t;
-        while(m>0){
-            sum+=a[k];
-            k++;
-            m--;
-        }
+            m=t;
+            while(m>0){
+                sum+=a[k];
+                m--;
+                k++;
+            }
         t++;
         }
-        b[i]=sum;
-    }
-    long long int max=b[0];
-    for(i=0;i<n;i++)
-    {
-        if(b[i]>max)
-            max=b[i];
+            if(i==0)
+                max=sum;
+            if(sum>max)
+                max=sum;
     }
     printf("%lld",max);
 }
