f5a99aa2239e82dc7dcbe48b49a3b1532a8e8b70
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2020-02-09 12:50:32 +0530

    Update Best Index.c

--- Best Index/Best Index.c
+++ Best Index/Best Index.c
@@ -2,44 +2,33 @@
 // PLZZ EXPLAIN ME WHY???
 
 #include<stdio.h>
-
-long long int bestindex(int a[],int n,int i)
-{
-    long long int sum=a[i],m=0,t=2;
-    while((i+t)<n)
-    {
-        while(m<t)
-        {
-            sum+=a[++i];
-            m++;
-        }t++;
-    }
-    return sum;
-}
-
-void max(long long int b[],long long int n)
-{
-    long long int m=b[0];
-    for(int i=0;i<n;i++)
-    {
-        if(b[i]>m)
-            m=b[i];
-    }
-    printf("%lld",m);
-}
-
 void main()
 {
     long long int n;
     scanf("%lld",&n);
-    long long int a[n],b[n];
+    long long int a[n],b[n],i;
     for(long long int i=0;i<n;i++)
     {
         scanf("%lld",&a[i]);
     }
-    for(int i=0;i<n;i++)
-    {
-        b[i]=bestindex(a,n,i);
+    for(i=0;i<n;i++){
+        long long int sum=0,t=1,m,k=i;
+        while((k+t)<=n){
+        m=t;
+        while(m>0){
+            sum+=a[k];
+            k++;
+            m--;
+        }
+        t++;
+        }
+        b[i]=sum;
     }
-    max(b,n);
+    long long int max=b[0];
+    for(i=0;i<n;i++)
+    {
+        if(b[i]>max)
+            max=b[i];
+    }
+    printf("%lld",max);
 }
